a9a3417b755760349b831a3795dd827d12cd7247
sh1okoh <kooooo.0719task@gmail.com>
2021-09-29 22:28:22 +0900

    webpackを利用してbundle したファイルを出力

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import $ from 'jquery'; // 相対パスにしなくて良い理由はwebpackが自動解決してくれているから
+import velocity from 'velocity-animate';
+import { add } from './modules/math'; // .jsがなくてもwebpackが自動で探しに行ってくれる
+
+console.log('app');
+
+const result = add(1, 2);
+
+$('body').append(result);
+velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  node: "development", // productionもある。devlopmentの方がbuildが早かったりする
-  entry: "./src/js/app.js",
+  mode: 'development', // productionもある。devlopmentの方がbuildが早かったりする
+  entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'public'), //出力先の設定、絶対パス
-    filename: "js/bundle.js", // 出力するファイル名
-  }
-}
+    filename: 'js/bundle.js', // 出力するファイル名
+  },
+};
