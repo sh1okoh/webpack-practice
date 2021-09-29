8755463ccb2253e350be9dfeec03e702d08e6ceb
sh1okoh <kooooo.0719task@gmail.com>
2021-09-29 22:36:51 +0900

    本番用と開発環境用に分けた

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // productionもある。devlopmentの方がbuildが早かったりする
+  // mode: 'development', // productionもある。devlopmentの方がbuildが早かったりする
   entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'public'), //出力先の設定、絶対パス
