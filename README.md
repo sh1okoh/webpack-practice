97a28e677e0fc9476b832518e853eae4be61d033
sh1okoh <kooooo.0719task@gmail.com>
2021-09-29 22:55:12 +0900

    ソースマップを出力するようにした

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # webpack-practice
 npm init -y
 
+ソースマップ
+bundle前と後のファイルを関連づける仕組み => デバッグがしやすくなる
+
--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,3 @@ const result = add(1, 2);
 
 $('body').append(result);
 velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
-
